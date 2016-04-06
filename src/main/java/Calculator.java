818843db22fc86b19c14292032684a7f8bd11b8e
ulme <ulmezz@gmail.com>
2016-04-06 11:44:57 +0200

    Added method subtraction to class Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -24,4 +24,8 @@ public class Calculator {
 	public double addition(double number1, double number2) {
 		return number1 + number2;
 	}
+	
+	public double subtraction(double number1, double number2) {
+		return number1 - number2;
+	}
 }
