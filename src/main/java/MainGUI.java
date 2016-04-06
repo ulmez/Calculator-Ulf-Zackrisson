5cd795c94992fed2c009ab13c8c43a8b2734a51f
ulme <ulmezz@gmail.com>
2016-04-06 12:57:56 +0200

    Have tested multiplication method in main method

--- src/main/java/MainGUI.java
+++ src/main/java/MainGUI.java
@@ -6,6 +6,7 @@ public class MainGUI {
 		
 		System.out.println(calc.addition(4.2, 2.2));
 		System.out.println(calc.subtraction(4.3, 3.8));
+		System.out.println(calc.multiplication(3.5, 2));
 	}
 
 }
