831f66f6b04207f80cb82dd860825ce75d3c01ad
ulme <ulmezz@gmail.com>
2016-04-06 11:47:19 +0200

    Added method division to class Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -32,4 +32,8 @@ public class Calculator {
 	public double multiplication(double number1, double number2) {
 		return number1 * number2;
 	}
+	
+	public double division(double number1, double number2) {
+		return number1 / number2;
+	}
 }
