4586014d2d8baca4ebf5c7cde63eeb4d044c0377
ulme <ulmezz@gmail.com>
2016-04-06 16:00:56 +0200

    Have inserted junit test method testSubtractionMethods

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,28 +11,58 @@ public class CalculatorTest {
 	
 	@BeforeClass
 	public static void beforeClass() {
-		System.out.println("BeforeClass");
+		//System.out.println("BeforeClass");
 	}
 	
 	@Before
 	public void before() {
 		calcTest = new Calculator();
-		System.out.println("Before");
+		//System.out.println("Before");
+	}
+	
+	// **************************************************
+	// Test of method addition **************************
+	@Test
+	public void testAdditionMethod1() {
+		assertEquals("Unexpected calculation of additionMethod1", 6.4, calcTest.addition(4.2, 2.2), 0);
 	}
 	
 	@Test
-	public void testAdditionMethod1() {
-		assertEquals("", 6.4, calcTest.addition(4.2, 2.2), 0);
-		System.out.println("This is the first test");
+	public void testAdditionMethod2() {
+		assertEquals("Unexpected calculation of additionMethod2", 10.4, calcTest.addition(-2.4, 12.8), 0);
 	}
+	
+	@Test
+	public void testAdditionMethod3() {
+		assertEquals("Unexpected calculation of additionMethod3", 244.6, calcTest.addition(56, 188.6), 0);
+	}
+	// **************************************************
+	
+	// **************************************************
+	// Test of method subtraction ***********************
+	@Test
+	public void testSubtractionMethod1() {
+		assertEquals("Unexpected calculation of subtractionMethod1", 2.0, calcTest.subtraction(4.2, 2.2), 0);
+	}
+	
+	@Test
+	public void testSubtractionMethod2() {
+		assertEquals("Unexpected calculation of subtractionMethod2", -14.4, calcTest.subtraction(-2.4, 12), 0);
+	}
+	
+	@Test
+	public void testSubtractionMethod3() {
+		assertEquals("Unexpected calculation of subtractionMethod3", -132.6, calcTest.subtraction(56, 188.6), 0);
+	}
+	// **************************************************
 	
 	@After
 	public void after() {
-		System.out.println("After");
+		//System.out.println("After");
 	}
 	
 	@AfterClass
 	public static void afterClass() {
-		System.out.println("AfterClass");
+		//System.out.println("AfterClass");
 	}
 }
