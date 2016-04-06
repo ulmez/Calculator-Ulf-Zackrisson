3ff68d00b0bd8b97b6ed3002ad4ef2b63e03ba23
ulme <ulmezz@gmail.com>
2016-04-06 12:59:24 +0200

    Have tested division method in main method

--- src/main/java/MainGUI.java
+++ src/main/java/MainGUI.java
@@ -7,6 +7,7 @@ public class MainGUI {
 		System.out.println(calc.addition(4.2, 2.2));
 		System.out.println(calc.subtraction(4.3, 3.8));
 		System.out.println(calc.multiplication(3.5, 2));
+		System.out.println(calc.division(22, 2.2));
 	}
 
 }
