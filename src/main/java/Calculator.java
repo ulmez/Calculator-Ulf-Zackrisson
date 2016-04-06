ac72ce47eb9c83a1df64c8595f643b99017f9514
ulme <ulmezz@gmail.com>
2016-04-06 12:52:56 +0200

    Instansiate Calculator and have made test of addition and subtraction method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,6 +3,9 @@ public class Calculator {
 	private double number1;
 	private double number2;
 	
+	public Calculator() {
+	}
+	
 	public Calculator(double number1, double number2) {
 		this.number1 = number1;
 		this.number2 = number2;
--- src/main/java/MainGUI.java
+++ src/main/java/MainGUI.java
@@ -2,8 +2,10 @@
 public class MainGUI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Calculator calc = new Calculator();
+		
+		System.out.println(calc.addition(4.2, 2.2));
+		System.out.println(calc.subtraction(4.3, 3.8));
 	}
 
 }
